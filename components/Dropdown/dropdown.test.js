c1b8c870d5aa9800b891691abdc23ee9aec7d612
John Robertson <jsrobertson@gmail.com>
2018-10-03 08:43:42 +0100

    Added setupTest file with enzyme adapter

--- components/Dropdown/dropdown.test.js
+++ components/Dropdown/dropdown.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Dropdown from '../Dropdown';
-
-Enzyme.configure({ adapter: new Adapter() });
+import { shallow } from 'enzyme';
+import Dropdown from '.';
 
 const user = {
   activeCaseLoadId: 'LEI',
