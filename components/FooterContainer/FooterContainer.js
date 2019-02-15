4a984289d739812f20c40b8db508e1c5e612ba2d
John Robertson <jsrobertson@gmail.com>
2019-02-15 14:50:08 +0000

    Removed extra slash

--- components/FooterContainer/FooterContainer.js
+++ components/FooterContainer/FooterContainer.js
@@ -4,31 +4,34 @@ import Footer from '../Footer'
 
 const FooterContainer = ({ feedbackEmail, prisonStaffHubUrl }) => {
   const link = (text, href, newTab) => ({ text, href, target: newTab ? '_blank' : undefined })
-
   const footerNavigation = [
     {
       title: 'Useful links',
       items: [
-        link('Features', `${prisonStaffHubUrl}/content/features`, true),
-        link('Roadmap', `${prisonStaffHubUrl}/content/roadmap`, true),
-        link('Support', `${prisonStaffHubUrl}/content/support`, true),
-        link("What's new", `${prisonStaffHubUrl}/content/whats-new`, true),
+        link('Features', `${prisonStaffHubUrl}content/features`, true),
+        link('Roadmap', `${prisonStaffHubUrl}content/roadmap`, true),
+        link('Support', `${prisonStaffHubUrl}content/support`, true),
+        link("What's new", `${prisonStaffHubUrl}content/whats-new`, true),
       ],
     },
   ]
-
   const footerMeta = {
     items: [
       { text: 'Contact', href: `mailto:${feedbackEmail}` },
-      link('Terms and conditions', `${prisonStaffHubUrl}/content/terms-conditions`, true),
+      link('Terms and conditions', `${prisonStaffHubUrl}content/terms-conditions`, true),
     ],
   }
+
   return <Footer meta={footerMeta} navigation={footerNavigation} />
 }
 
 FooterContainer.propTypes = {
   feedbackEmail: PropTypes.string.isRequired,
-  prisonStaffHubUrl: PropTypes.string.isRequired,
+  prisonStaffHubUrl: PropTypes.string,
+}
+
+FooterContainer.defaultProps = {
+  prisonStaffHubUrl: undefined,
 }
 
 export default FooterContainer
