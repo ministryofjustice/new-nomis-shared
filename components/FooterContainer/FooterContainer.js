daf4ebeaae57e0726e64d3e0d04c368df457e5e6
John Robertson <jsrobertson@gmail.com>
2019-06-04 10:28:18 +0100

    Delete our Footer and update FooterContainer to use govuk-react/footer

--- components/FooterContainer/FooterContainer.js
+++ components/FooterContainer/FooterContainer.js
@@ -1,28 +1,67 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Footer from '../Footer'
+import styled from 'styled-components'
+import Footer from '@govuk-react/footer'
+import { MEDIA_QUERIES, GUTTER, GUTTER_HALF, GUTTER_PX } from '@govuk-react/constants'
+import crest from './images/govuk-crest-2x.png'
 
-const FooterContainer = ({ feedbackEmail, prisonStaffHubUrl }) => {
-  const link = (text, href, newTab) => ({ text, href, target: newTab ? '_blank' : undefined })
-  const footerNavigation = [
-    {
-      title: 'Useful links',
-      items: [
-        link('Features', `${prisonStaffHubUrl}content/features`, true),
-        link('Roadmap', `${prisonStaffHubUrl}content/roadmap`, true),
-        link('Support', `${prisonStaffHubUrl}content/support`, true),
-        link("What's new", `${prisonStaffHubUrl}content/whats-new`, true),
-      ],
-    },
-  ]
-  const footerMeta = {
-    items: [
-      { text: 'Contact', href: `mailto:${feedbackEmail}` },
-      link('Terms and conditions', `${prisonStaffHubUrl}content/terms-conditions`, true),
-    ],
+const MAX_WIDTH_PX = 1170
+const MAX_WIDTH = `${MAX_WIDTH_PX}px`
+
+const WideContainer = styled(Footer.WidthContainer)`
+  max-width: ${MAX_WIDTH};
+  margin: 0 ${GUTTER_HALF};
+
+  ${MEDIA_QUERIES.TABLET} {
+    margin: 0 ${GUTTER};
   }
 
-  return <Footer meta={footerMeta} navigation={footerNavigation} />
+  @media screen and (min-width: ${MAX_WIDTH_PX + GUTTER_PX * 2}px) {
+    margin: 0 auto;
+  }
+`
+
+const FooterContainer = ({ feedbackEmail, prisonStaffHubUrl }) => {
+  const copyright = {
+    text: 'Crown copyright',
+    link:
+      'https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/',
+    image: {
+      source: crest,
+      height: 102,
+      width: 125,
+    },
+  }
+
+  const meta = (
+    <Footer.MetaLinks heading="Support links">
+      <Footer.Link href={`mailto:${feedbackEmail}`}>Contact</Footer.Link>
+      <Footer.Link href={`${prisonStaffHubUrl}content/terms-conditions`} target="_blank">
+        Terms and conditions
+      </Footer.Link>
+    </Footer.MetaLinks>
+  )
+
+  return (
+    <Footer copyright={copyright} meta={meta} container={WideContainer}>
+      <Footer.Navigation>
+        <Footer.NavigationLinks heading="Useful links">
+          <Footer.Link href={`${prisonStaffHubUrl}content/features`} target="_blank">
+            Features
+          </Footer.Link>
+          <Footer.Link href={`${prisonStaffHubUrl}content/roadmap`} target="_blank">
+            Roadmap
+          </Footer.Link>
+          <Footer.Link href={`${prisonStaffHubUrl}content/support`} target="_blank">
+            Support
+          </Footer.Link>
+          <Footer.Link href={`${prisonStaffHubUrl}content/whats-new`} target="_blank">
+            What&apos;s new
+          </Footer.Link>
+        </Footer.NavigationLinks>
+      </Footer.Navigation>
+    </Footer>
+  )
 }
 
 FooterContainer.propTypes = {
---
+++ stories/FooterContainer/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Footer from '../../components/FooterContainer'
+
+storiesOf('Footer', module).add('Default footer container', () => <Footer />)
